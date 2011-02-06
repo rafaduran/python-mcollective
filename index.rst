a121bdba8716fe2e05e0700fbc744fc5eaee0822
Aaron Brady <bradya@gmail.com>
2011-02-06 22:12:21 +0000

    Document what we have so far.

---
+++ index.rst
@@ -0,0 +1,22 @@
+Python MCollective RPC Client
+=============================
+
+mcollective-python is an attempt to build Python compatible RPC bindings for
+PuppetLabs' MCollective.
+
+Only an RPC client is included.
+
+API Documentation
+-----------------
+
+.. toctree::
+   :maxdepth: 2
+
+.. automodule:: mcollective
+  :members:
+
+.. autoexception:: AlreadySentException
+  :members:
+
+.. autoclass:: Message
+  :members: __init__, sign, subscribe_to_replies, send, collect_results, send_and_await_results
--- mcollective.py
+++ mcollective.py
@@ -7,6 +7,13 @@ class AlreadySentException(Exception):
 
 class Message:
     def __init__(self, body, stomp_client, prefix='mcollective', target='rpcutil'):
+        '''Create a new message.
+        
+        :param body: Correctly encoded RPC message.
+        :type body: dict
+        :param stomp_client: Open connection to STOMP server
+        :param prefix: MCollective prefix
+        :param target: MCollective target'''
         self.stomp_client = stomp_client
         self.target = '/topic/%s.%s.command' % (prefix, target)
         self.target_reply = '/topic/%s.%s.reply' % (prefix, target)
@@ -26,15 +33,31 @@ class Message:
         self.request = r
     
     def subscribe_to_replies(self):
+        '''Subscribe to the response topic for this message.
+        
+        You should run this before sending the message, if you are interested
+        in the replies from agents.'''
         self.stomp_client.subscribe(self.target_reply)
     
     def sign(self, private_key, certificate_name, sender_id='python'):
+        '''Sign the body of the message.
+
+        :param private_key: an RSA object with a private key loaded.
+        :type private_key: M2Crypto.RSA.RSA
+        :param certificate_name: the name of the matching cert (as stored on the agents).
+        :type certificate_name: str'''
         self.request[":callerid"] = 'cert=%s' % certificate_name
         self.request[":senderid"] = sender_id
         hashed_signature = private_key.sign(sha1(self.request[':body']).digest(), 'sha1')
         self.request[':hash'] = hashed_signature.encode('base64').replace("\n", "").strip()
 
     def send(self, debug=False):
+        '''Send the encoded message to the target topic
+
+        :param debug: if true, the message will be printed to standard error.
+        :type debug: boolean
+        
+        You can only send each message once. Subsequent calls will raise :class:`AlreadySentException`'''
         if self.sent:
             raise AlreadySentException()
         data = dump(self.request, explicit_start=True, explicit_end=False)
@@ -45,6 +68,9 @@ class Message:
         self.sent = True
     
     def collect_results(self):
+        '''Collect the results from a previous :func:`Message.send` call.
+        
+        :rtype: list of STOMP messages which match this object's `:requestid`'''
         results = []
         while True:
             message = None
@@ -58,6 +84,13 @@ class Message:
         return results
     
     def send_and_await_results(self, timeout=2, debug=False):
+        '''Send the prepared RPC message, wait a set period and collect the results.
+
+        :param timeout: Time to wait for a response.
+        :type timeout: int
+        :param debug: if true, the message will be printed to standard error.
+        :type debug: boolean
+        :rtype: list of STOMP messages which match this object's `:requestid`'''
         self.subscribe_to_replies()
         self.send(debug=debug)
         sleep(timeout)
