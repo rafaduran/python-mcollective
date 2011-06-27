516c3cca1dda8e1bbf20c693a5ef4b11169c5347
Aaron Brady <bradya@gmail.com>
2011-06-27 15:26:00 +0100

    Switch check_mc to NPRE module, not Icinga one

--- check_mc.py
+++ check_mc.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
-from mcollective import Message
+from mcollective import Message, Filter
 from yaml import load, dump
 from M2Crypto.RSA import load_pub_key, load_key
 from stompy.simple import Client
 from sys import argv, exit, stderr
+import os.path
 from math import floor
 
 config = {}
-execfile('/etc/check_mc.rc', config)
+for line in file('/etc/mcollective/client.cfg', 'rt').readlines():
+    bits = line.split('=', 1)
+    if len(bits) == 2:
+        config[bits[0].strip()] = bits[1].strip()
 
 if len(argv) < 2:
     print >>stderr, '%s <check> [args]' % argv[0]
     exit(-1)
 
-certificate_name = config['certificate_name']
-stomp_client = Client(config.get('host', 'localhost'), port=config.get('port', 61613))
-stomp_client.connect(config['username'], config['password'])
-private_key = load_key(config['private_key'])
+certificate_name = os.path.basename(config['plugin.ssl_client_public'].replace('.pem', ''))
+stomp_client = Client(config.get('plugin.stomp.host', 'localhost'), port=int(config.get('plugin.stomp.port', 61613)))
+stomp_client.connect(config['plugin.stomp.user'], config['plugin.stomp.password'])
+private_key = load_key(config['plugin.ssl_client_private'])
 
 args = ' '.join(argv[2:])
 
@@ -24,26 +28,18 @@ body = {
     ':caller': 'cert=%s' % certificate_name, 
     ':data': {
         ':process_results': True, 
-        ':args': args, 
-        ':plugin': argv[1],
+        ':command': argv[2],
     }, 
     ':action': 'runcommand', 
-    ':agent': 'icinga'
+    ':agent': 'nrpe'
 }
 
-m = Message(body, stomp_client, target='icinga')
+m = Message(body, stomp_client, target='nrpe', filter_=Filter(identity=argv[1]).dump())
 m.sign(private_key, certificate_name)
-results = m.send_and_await_results(timeout=config.get('timeout', 5))
-
+results = m.send_and_await_results(timeout=1)
 data = [load(q[':body'])[':data'] for q in results]
-
-our_output = " - ".join([o[':output'] for o in data])
-exit_codes = [o[':exitcode'] for o in data]
-
-try:
-    our_exit_code = int(floor(sum(exit_codes, 0.0) / len(exit_codes)))
-except ZeroDivisionError:
-    our_exit_code = 3
-
-print our_output
-exit(our_exit_code)
+if len(data):
+    print data[0][':output']
+    exit(int(data[0][':exitcode']))
+print 'No response from MCollective'
+exit(3)
