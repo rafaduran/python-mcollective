4a2e676392b8bac66e6249d62d4695b79c264850
Darren Worrall <darren@iweb.co.uk>
2011-07-05 12:42:39 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import sys
+import mcollective
+
+
+setup(name='mcollective',
+      version='0.1',
+      author='Aaron Brady',
+      author_email='bradya@gmail.com',
+      url='https://github.com/insom/mcollective-python',
+      description="Python library for using Marionette Collective's RPC",
+      #long_description=mcollective.__doc__,
+      py_modules=['mcollective',],
+      provides=['mcollective',],
+      keywords='mcollective',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2',
+                  ],
+     )
