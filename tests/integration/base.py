0eabf901ec3eb52bd4cc9b9202cbf5e633859e37
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2013-05-12 19:33:06 +0200

    Added CoilMQ integration tests support
    It will help setup a simple Python Stomp server for integration tests.
    For now I start/stop the server for each server, I will review this
    later.

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -1,10 +1,17 @@
+import threading
 import os
+import unittest
 
+from coilmq.auth import simple
+from coilmq.tests import functional
 import git
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 VENDOR_PATH = os.path.join(ROOT, 'integration/vendor')
 VENDOR = 'https://github.com/puppetlabs/marionette-collective.git'
+
+BIND_ADDRESS = '127.0.0.1'
+BIND_PORT = 6163
 
 
 class IntegrationTestCaseMixin(object):
@@ -32,3 +39,91 @@ class IntegrationTestCaseMixin(object):
         repo.head.reference = reference
         repo.head.reset(index=True, working_tree=True)
         return repo
+
+
+# Most CoilMQ code is borrowed from CoilMQ functional tests
+class TestWithCoilMQ(IntegrationTestCaseMixin):
+    def setup(self):
+        self.clients = []
+        self.server = None  # This gets set in the server thread.
+        self.server_address = None # This gets set in the server thread.
+        self.ready_event = threading.Event()
+
+        addr_bound = threading.Event()
+        def start_server():
+            self.server = functional.TestStompServer(
+                (BIND_ADDRESS, BIND_PORT),
+                ready_event=self.ready_event,
+                authenticator=simple.SimpleAuthenticator(
+                    store={'mcollective': 'secret'}),
+                queue_manager=self._queuemanager(),
+                topic_manager=self._topicmanager())
+            self.server_address = self.server.socket.getsockname()
+            addr_bound.set()
+            self.server.serve_forever()
+
+        self.server_thread = threading.Thread(target=start_server, name='server')
+        self.server_thread.start()
+        self.ready_event.wait()
+        addr_bound.wait()
+
+    def _queuemanager(self):
+        """
+        Returns the configured L{QueueManager} instance to use.
+
+        Can be overridden by subclasses that wish to change out any queue mgr parameters.
+
+        @rtype: L{QueueManager}
+        """
+        return functional.QueueManager(
+            store=functional.MemoryQueue(),
+            subscriber_scheduler=functional.FavorReliableSubscriberScheduler(),
+            queue_scheduler=functional.RandomQueueScheduler())
+
+    def _topicmanager(self):
+        """
+        Returns the configured L{TopicManager} instance to use.
+
+        Can be overridden by subclasses that wish to change out any topic mgr parameters.
+
+        @rtype: L{TopicManager}
+        """
+        return functional.TopicManager()
+
+    def teardown(self):
+        for c in self.clients:
+            print "Disconnecting %s" % c
+            c.close()
+        self.server.shutdown()
+        self.server_thread.join()
+        self.ready_event.clear()
+        del self.server_thread
+
+    def _new_client(self, connect=True):
+        """
+        Get a new L{TestStompClient} connected to our test server.
+
+        The client will also be registered for close in the tearDown method.
+
+        @param connect: Whether to issue the CONNECT command.
+        @type connect: C{bool}
+
+        @rtype: L{TestStompClient}
+        """
+        client = functional.TestStompClient(self.server_address)
+        self.clients.append(client)
+        if connect:
+            client.connect()
+            r = client.received_frames.get(timeout=1)
+            assert r.command == 'CONNECTED'
+        return client
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.setup()
+
+    def tearDown(self):
+        self.teardown()
+        super(TestCase, self).tearDown()
---
+++ tests/integration/test_with_coilmq.py
@@ -0,0 +1,11 @@
+import unittest
+
+import base
+
+
+class CoilMQIntegrationTests(base.TestCase, base.TestWithCoilMQ):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
