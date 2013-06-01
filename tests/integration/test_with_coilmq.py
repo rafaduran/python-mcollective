740a45d2dddbb5506f4e293abd97d5bec3c80889
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2013-06-01 14:35:56 +0200

    Integration testcases for MCollective 2.0.x/2.2.x

--- tests/integration/test_with_coilmq.py
+++ tests/integration/test_with_coilmq.py
@@ -3,3 +3,27 @@ import base
 
 class TestWithCoilMQIntegration(base.TestCase, base.CoilMQIntegration):
     pass
+
+
+class TestWithCoilMQ22x(TestWithCoilMQIntegration):
+    def setup(self):
+        self.get_vendor_rev('2.2.x')
+        super(TestWithCoilMQ22x, self).setup()
+
+    def teardown(self):
+        base.TestCase.teardown(self)
+
+    def test_something(self):
+        assert True
+
+
+class TestWithCoilMQ20x(TestWithCoilMQIntegration):
+    def setup(self):
+        self.get_vendor_rev('2.0.x')
+        super(TestWithCoilMQ20x, self).setup()
+
+    def teardown(self):
+        base.TestCase.teardown(self)
+
+    def test_something(self):
+        assert True
