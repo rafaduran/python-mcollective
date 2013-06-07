76cb8fb6239c7ac062f786467b8cf969e3c46943
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2013-06-08 01:38:58 +0200

    More script fixes (RabbitMQ plugins needs sudo)

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eu
-rabbitmq-plugins enable rabbitmq_stomp
+sudo rabbitmq-plugins enable rabbitmq_stomp
 # Each MCollective collective requires two exchanges, direct and broadcast
 # In this case just `mcollective` collective
 `dirname $0`/rabbitmqadmin declare exchange name=mcollective_directed type=direct
 `dirname $0`/rabbitmqadmin declare exchange name=mcollective_broadcast type=topic
-service rabbitmq-server restart  # so changes make effect
+sudo service rabbitmq-server restart  # so changes make effect
