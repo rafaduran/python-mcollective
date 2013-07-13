17897b1bf36671e1c8902f6d08c4ead3df677661
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2013-07-13 14:55:07 +0200

    Default context as a dict
    So we create configuration from it (whenever config objects accepts a
    dict-like object).

--- tests/base.py
+++ tests/base.py
@@ -5,35 +5,28 @@ import jinja2
 
 ROOT = os.path.abspath(os.path.join(__file__, '..'))
 DEFAULT_CTXT = {
-    'topic': 'topic',
+    'topicprefix': 'topic',
     'collectives': ['mcollective', 'sub1', 'sub2'],
-    'maincollective': 'mcollective',
-    'root': ROOT,
+    'main_collective': 'mcollective',
+    'libdir': '{root}/integration/vendor/plugins'.format(root=ROOT),
+    'logfile': '{root}/mcollective.log'.format(root=ROOT),
     'loglevel': 'debug',
     'daemonize': 0,
     'identity': 'mco1',
-    'securityprovider': {
-        'name': 'ssl',
-        'options': {
-            'ssl_server_public': 'mcserver-public.pem',
-            'ssl_client_private': '{root}/fixtures/testkey-private.pem'.format(
-                root=ROOT),
-            'ssl_client_public': '{root}/fixtures/testkey-public.pem'.format(
-                root=ROOT),
-            'ssl_serializer': 'yaml',
-        }
-    },
-    'connector': {
-        'name': 'activemq',
-        'options': {
-            'pool.size': '1',
-            'pool.1.host': 'localhost',
-            'pool.1.port': '6163',
-            'pool.1.user': 'mcollective',
-            'pool.1.password': 'secret',
-            'pool.1.ssl': 'false',
-        },
-    },
+    'securityprovider': 'ssl',
+    'plugin.ssl_server_public': 'mcserver-public.pem',
+    'plugin.ssl_client_private': '{root}/fixtures/testkey-private.pem'.format(
+        root=ROOT),
+    'plugin.ssl_client_public': '{root}/fixtures/testkey-public.pem'.format(
+        root=ROOT),
+    'plugin.ssl_serializer': 'yaml',
+    'connector': 'activemq',
+    'plugin.activemq.pool.size': '1',
+    'plugin.activemq.pool.1.host': 'localhost',
+    'plugin.activemq.pool.1.port': '6163',
+    'plugin.activemq.pool.1.user': 'mcollective',
+    'plugin.activemq.pool.1.password': 'secret',
+    'plugin.activemq.pool.1.ssl': 'false',
 }
 TEST_CFG = os.path.join(ROOT, 'server.cfg')
 
@@ -47,5 +40,5 @@ def configfile(ctxt=None):
     if not ctxt:
         ctxt = DEFAULT_CTXT
     with open(TEST_CFG, 'wt') as cfg:
-        cfg.write(get_template('server.cfg.jinja').render(ctxt))
+        cfg.write(get_template('server.cfg.jinja').render({'config': ctxt}))
     return TEST_CFG
