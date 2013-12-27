db8fe3051250ffe5678fe5b63b98115d86bf1830
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2013-12-27 09:19:07 +0100

    Travis support for integration tests

---
+++ scripts/activemq.sh
@@ -0,0 +1,23 @@
+ROOT="`dirname $0`/.."
+ROOT=`readlink -e $ROOT`
+
+cat << EOF > /tmp/dna.json
+{
+  "run_list": [
+    "recipe[activemq]",
+    "recipe[activemq_mco]"
+  ]
+}
+EOF
+
+cat << EOF > /tmp/solo.rb
+file_cache_path    "/var/chef/cache"
+file_backup_path   "/var/chef/backup"
+cookbook_path ["${ROOT}/cookbooks/ci_environment", "${ROOT}/extra_cookbooks"]
+log_level :debug
+verbose_logging    false
+
+encrypted_data_bag_secret "/tmp/encrypted_data_bag_secret"
+EOF
+
+chef-solo -c /tmp/solo.rb -j /tmp/dna.json
