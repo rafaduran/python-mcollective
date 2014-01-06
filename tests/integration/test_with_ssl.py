b9b3837937341e6b1b052bbfdd979e3bb57d87c4
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2014-01-07 00:43:47 +0100

    Fix SSL security provider integration tests
    They were running with none provider instead.

--- tests/integration/test_with_ssl.py
+++ tests/integration/test_with_ssl.py
@@ -1,4 +1,9 @@
+import os
+
+from pymco.test import ctxt
 from . import base
+
+FIXTURES_PATH = os.path.join(ctxt.ROOT, 'fixtures')
 
 
 class SSLTestCase(base.IntegrationTestCase):
@@ -9,6 +14,10 @@ class SSLTestCase(base.IntegrationTestCase):
         'plugin.ssl_server_public': 'tests/fixtures/server-public.pem',
         'plugin.ssl_client_private': 'tests/fixtures/client-private.pem',
         'plugin.ssl_client_public': 'tests/fixtures/client-public.pem',
+        'plugin.ssl_server_private': os.path.join(FIXTURES_PATH,
+                                                  'server-private.pem'),
+        'securityprovider': 'ssl',
+        'plugin.ssl_client_cert_dir': FIXTURES_PATH,
     }
 
 
