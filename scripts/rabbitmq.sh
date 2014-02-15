e0fdd635d50413118bb201e9175fc10e21da66e5
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2014-02-15 09:41:48 +0100

    RabbitMQ SSL setup for Travis

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -6,4 +6,10 @@ sudo apt-get install python-pip -y
 pip install requests --use-mirrors
 sudo rabbitmq-plugins enable rabbitmq_stomp
 ${ROOT}/rabbitmq.py
+sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/ca.pem \
+    /opt/rabbitmq/ca.pem
+sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/cert.pem \
+    /opt/rabbitmq/cert.pem
+sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/key.pem \
+    /opt/rabbitmq/key.pem
 sudo service rabbitmq-server restart  # so changes make effect
