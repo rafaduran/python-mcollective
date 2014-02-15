82cc3ad33831a1dee6d1ce155d2a982db3ad0baf
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2014-02-15 10:05:19 +0100

    Fix RabbitMQ setup on Travis

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -6,10 +6,13 @@ sudo apt-get install python-pip -y
 pip install requests --use-mirrors
 sudo rabbitmq-plugins enable rabbitmq_stomp
 ${ROOT}/rabbitmq.py
-sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/ca.pem \
-    /opt/rabbitmq/ca.pem
-sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/cert.pem \
-    /opt/rabbitmq/cert.pem
-sudo cp ${ROOT}/extra_cookbooks/rabbitmq_mco/templates/default/key.pem \
-    /opt/rabbitmq/key.pem
+sudo cp ${ROOT}/../extra_cookbooks/rabbitmq_mco/templates/default/rabbitmq.config.erb \
+    /etc/rabbitmq/rabbitmq.config
+sudo sed -i 's/<%= node.rabbitmq.high_memory_watermark %>/0.04/' /etc/rabbitmq/rabbitmq.config
+sudo cp ${ROOT}/../extra_cookbooks/rabbitmq_mco/templates/default/ca.pem \
+    /etc/rabbitmq/ca.pem
+sudo cp ${ROOT}/../extra_cookbooks/rabbitmq_mco/templates/default/cert.pem \
+    /etc/rabbitmq/cert.pem
+sudo cp ${ROOT}/../extra_cookbooks/rabbitmq_mco/templates/default/key.pem \
+    /etc/rabbitmq/key.pem
 sudo service rabbitmq-server restart  # so changes make effect
