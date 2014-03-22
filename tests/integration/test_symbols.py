71147e2ce3c10478277be6f64a0f348d075d581f
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2014-03-22 19:01:59 +0100

    Add Ruby symbols serialization integration test

---
+++ tests/integration/test_symbols.py
@@ -0,0 +1,40 @@
+from pymco import message
+from pymco import rpc
+from pymco.test import ctxt as test_ctxt
+import pytest
+
+from . import base
+
+
+class TestMCllectiveSymbols(base.BaseIntegrationTestCase, base.MCollectiveCurrent):
+    CTXT = {
+        'plugin.activemq.pool.1.port': 61614,
+        'plugin.activemq.pool.1.password': 'marionette',
+    }
+
+    @pytest.mark.timeout(method='thread', timeout=15)
+    def test_symbols(self):
+        body = {
+            ':agent': 'demo',
+            ':action': 'mounts',
+            ':caller': 'user=rafaduran',
+            ':data': {':process_results': True},
+        }
+        msg = message.Message(body=body, agent='demo', config=self.config)
+        simple_rpc = rpc.SimpleAction(msg=msg,
+                                      agent='demo',
+                                      action='mounts',
+                                      config=self.config)
+        result = simple_rpc.call()
+        assert len(result) == 1
+        msg = result[0]
+        assert msg[':senderagent'] == 'demo'
+        assert msg[':requestid'] == msg[':requestid']
+
+    def get_ctxt(self):
+        ctxt = super(TestMCllectiveSymbols, self).get_ctxt()
+        ctxt['libdir'] = ':'.join([
+            ctxt['libdir'],
+            '{root}/fixtures/plugins'.format(root=test_ctxt.ROOT),
+        ])
+        return ctxt
