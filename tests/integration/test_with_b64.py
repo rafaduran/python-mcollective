fdc421c5c45ad5d57f21adf9ac753071d365b5c9
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2014-07-20 20:13:27 +0200

    Cosmetic + pep8 changes

--- tests/integration/test_with_b64.py
+++ tests/integration/test_with_b64.py
@@ -7,25 +7,25 @@ FIXTURES_PATH = os.path.join(ctxt.ROOT, 'fixtures')
 
 
 class Base64TestCase(base.IntegrationTestCase):
-    '''ActiveMQ integration test case.'''
+    """ActiveMQ integration test case."""
     CTXT = {
         'plugin.activemq.pool.1.port': 61614,
         'plugin.activemq.pool.1.password': 'marionette',
-        'plugin.activemq.pool.1.base64': 'yes'
+        'plugin.activemq.pool.1.base64': 'yes',
     }
 
+
 class TestWithBase64MCo20x(base.MCollective20x, Base64TestCase):
-    '''MCollective integration test case.'''
+    """MCollective integration test case."""
 
 
 class TestWithBase64MCo22x(base.MCollective22x, Base64TestCase):
-    '''MCollective integration test case.'''
+    """MCollective integration test case."""
 
 
 class TestWithBase64MCo23x(base.MCollective23x, Base64TestCase):
-    '''MCollective integration test case.'''
+    """MCollective integration test case."""
 
 
 class TestWithBase64MCo24x(base.MCollective24x, Base64TestCase):
     """MCollective integration test case."""
-
